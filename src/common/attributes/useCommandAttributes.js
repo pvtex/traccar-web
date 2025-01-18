cbe736ba93032efef9bf284727980a905c2ca896
PVTEX <info@pvtex.de>
2025-01-18 20:53:43 +0100

    commad status led

--- src/common/attributes/useCommandAttributes.js
+++ src/common/attributes/useCommandAttributes.js
@@ -224,4 +224,11 @@ export default (t) => useMemo(() => ({
       type: 'number',
     },
   ],
+  statusLED: [
+    {
+      key: 'enable',
+      name: t('commandStatusLed'),
+      type: 'boolean',
+    },
+  ],
 }), [t]);
